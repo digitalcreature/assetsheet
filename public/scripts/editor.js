9665d9dbed23784b9cee417fe6fe1d40ad712c51
Tanner Grehawick <tgrehawi@gmail.com>
2017-09-19 20:13:53 -0400

    implemented scroll thingy (from that jsfiddle thing)

--- public/scripts/editor.js
+++ public/scripts/editor.js
@@ -1,2 +1,8 @@
 $(document).ready(function() {
+	$(".editor").scroll(function() {
+  	var left = $(this).scrollLeft();
+    var top = $(this).scrollTop();
+    $(".cell.first").css("left", left);
+    $(".row.header").css("top", top);
+  });
 });
